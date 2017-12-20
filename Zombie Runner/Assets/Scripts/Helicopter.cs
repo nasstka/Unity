6071529a5ec7f5605848fe447fcc8b74d6e2ecfb
Nastka <nastka.myslewska@gmail.com>
2017-12-20 18:23:31 +0100

    Helicopter

---
+++ Zombie Runner/Assets/Scripts/Helicopter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour
+{
+    public AudioClip callSound;
+
+    private AudioSource audioSource;
+    private bool called = false;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Heli") && !called)
+        {  
+            called = true;
+            Debug.Log("Called");
+            audioSource.clip = callSound;
+            audioSource.Play();
+        }
+    }
+}
---
+++ Zombie Runner/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // The parent of spawn points
+    public Transform playerSpawnPoints;
+    public bool reSpawn = false;
+
+    private Transform[] spawnPoints;
+    private bool lastToggle = false;
+
+    void Start()
+    {
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (lastToggle != reSpawn)
+        {
+            Respawn();
+            reSpawn = false;
+        }
+        else
+        {
+            lastToggle = reSpawn;
+        }
+    }
+
+    private void Respawn()
+    {
+        int indx = Random.Range(1, spawnPoints.Length);
+
+        transform.position = spawnPoints[indx].transform.position;
+    }
+}
